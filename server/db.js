dbefe9fb0d716739de51b32514bacef0bbd5008b
SHUBHAM KUMAR <shubhamkumarsinghbba@gmail.com>
2024-01-09 23:19:23 +0530

    hot-commit

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     password: process.env.PASSWORD,
     host: process.env.HOST,
     port: process.env.DBPORT,
-    database: 'todoapp'
+    database: 'todo_database_psct'
 });
 
 module.exports = pool;
